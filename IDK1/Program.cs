3b8bb3cf44dde8a152d9329649febf8cdf3a5877
DuckBoy102 <avieln7@gmail.com>
2022-03-24 16:14:55 +0200

    hay

---
+++ IDK1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IDK1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] grades = new int[5];
+            int i;
+            int average = 0;
+            Console.WriteLine("please enter " + grades.Length + " grades:");
+            for (i = 0; i < grades.Length; i++)
+                grades[i] = int.Parse(Console.ReadLine());
+
+            for (i = 0; i < grades.Length; i++)
+                average += grades[i];
+            average = average / grades.Length;
+            Console.WriteLine("the average is {0}", average);
+        }
+    }
+}
